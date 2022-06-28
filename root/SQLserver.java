36844bab7a51436f078eafdb4b865b441e801865
Tuan Dung Nguyen <dzung.csu@gmail.com>
2022-06-28 11:46:28 -0600

    query can now access tables from different databases using only one connection

--- root/SQLserver.java
+++ root/SQLserver.java
@@ -16,7 +16,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
@@ -59,174 +58,163 @@ public class SQLserver {
 		String combine_st = "";
 		
 		// Connect to your database.
-		List<String> databases = Arrays.asList("SIT2015", "SIT2016");
-		for (String database : databases) {
-			String conn_SIT2015 = "jdbc:sqlserver://localhost:1433;databaseName=" + database + ";integratedSecurity=true";
-			ResultSet resultSet = null;
-			try (Connection connection = DriverManager.getConnection(conn_SIT2015);
-					Statement statement = connection.createStatement();
-					) {
-				// Create and execute a SELECT SQL statement.
-				String selectSql = "SELECT [INC209R_IDENTIFIER], [LIFE_SAFETY_HEALTH_STATUS_NARR] FROM [SIT209_HISTORY_INCIDENT_209_REPORTS]";
-//				String selectSql = "SELECT [CURRENT_THREAT_12]FROM [SIT209_HISTORY_INCIDENT_209_REPORTS]";
-
-				resultSet = statement.executeQuery(selectSql);
-				while (resultSet.next()) {
-					INC209R.add(database + "." + resultSet.getString(1));
-					String st = resultSet.getString(2);
-					combine_st = combine_st + " " + st;
-					row_data.add(st);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+		// Single connection can work the same as the below multiple connections 
+		ResultSet resultSet = null;
+		String conn_SIT2015 = "jdbc:sqlserver://localhost:1433;databaseName=SIT2015;integratedSecurity=true";
+		try (Connection connection = DriverManager.getConnection(conn_SIT2015);
+				Statement statement = connection.createStatement();
+				) {
+			// Create and execute a SELECT SQL statement.
+			String selectSql = 
+					"""
+					SELECT 2015, [INC209R_IDENTIFIER], [LIFE_SAFETY_HEALTH_STATUS_NARR] FROM [SIT2015].[dbo].[SIT209_HISTORY_INCIDENT_209_REPORTS]
+					UNION
+					SELECT 2016, [INC209R_IDENTIFIER], [LIFE_SAFETY_HEALTH_STATUS_NARR] FROM [SIT2016].[dbo].[SIT209_HISTORY_INCIDENT_209_REPORTS]
+					""";
+			resultSet = statement.executeQuery(selectSql);
+			while (resultSet.next()) {
+				INC209R.add(resultSet.getString(1) + "." + resultSet.getString(2));
+				String st = resultSet.getString(3);
+				combine_st = combine_st + " " + st;
+				row_data.add(st);
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
-//		String conn_SIT2015 = "jdbc:sqlserver://localhost:1433;databaseName=SIT2015;integratedSecurity=true";
-//		String conn_SIT2016 = "jdbc:sqlserver://localhost:1433;databaseName=SIT2016;integratedSecurity=true";
-//		ResultSet resultSet1 = null;
-//		ResultSet resultSet2 = null;
-//		try (Connection connection1 = DriverManager.getConnection(conn_SIT2015);
-//				Connection connection2 = DriverManager.getConnection(conn_SIT2016);
-//				Statement statement1 = connection1.createStatement();
-//				Statement statement2 = connection2.createStatement();
-//				) {
-//			// Create and execute a SELECT SQL statement.
-//			String selectSql = "SELECT [INC209R_IDENTIFIER], [LIFE_SAFETY_HEALTH_STATUS_NARR] FROM [SIT209_HISTORY_INCIDENT_209_REPORTS]";
-////			String selectSql = "SELECT [CURRENT_THREAT_12]FROM [SIT209_HISTORY_INCIDENT_209_REPORTS]";
+//		// Multiple connection		
+//		List<String> databases = Arrays.asList("SIT2015", "SIT2016");
+//		for (String database : databases) {
+//			String conn_SIT2015 = "jdbc:sqlserver://localhost:1433;databaseName=" + database + ";integratedSecurity=true";
+//			ResultSet resultSet = null;
+//			try (Connection connection = DriverManager.getConnection(conn_SIT2015);
+//					Statement statement = connection.createStatement();
+//					) {
+//				// Create and execute a SELECT SQL statement.
+//				String selectSql = "SELECT [INC209R_IDENTIFIER], [LIFE_SAFETY_HEALTH_STATUS_NARR] FROM [SIT209_HISTORY_INCIDENT_209_REPORTS]";
+////				String selectSql = "SELECT [CURRENT_THREAT_12]FROM [SIT209_HISTORY_INCIDENT_209_REPORTS]";
 //
-//			List<String> INC209R = new ArrayList<String>();
-//			List<String> row_data = new ArrayList<String>();
-//			String combine_st = "";
-//			
-//			resultSet1 = statement1.executeQuery(selectSql);
-//			while (resultSet1.next()) {
-//				INC209R.add("2015." + resultSet1.getString(1));
-//				String st = resultSet1.getString(2);
-//				combine_st = combine_st + " " + st;
-//				row_data.add(st);
+//				resultSet = statement.executeQuery(selectSql);
+//				while (resultSet.next()) {
+//					INC209R.add(database + "." + resultSet.getString(1));
+//					String st = resultSet.getString(2);
+//					combine_st = combine_st + " " + st;
+//					row_data.add(st);
+//				}
+//			} catch (SQLException e) {
+//				e.printStackTrace();
 //			}
-//			
-//			resultSet2 = statement2.executeQuery(selectSql);
-//			while (resultSet2.next()) {
-//				INC209R.add("2016." + resultSet2.getString(1));
-//				String st = resultSet2.getString(2);
-//				combine_st = combine_st + " " + st;
-//				row_data.add(st);
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
 //		}
 		
-			// Apache Lucene: https://stackoverflow.com/questions/17447045/java-library-for-keywords-extraction-from-input-text
-			try {	// Identify Keywords and frequency
-				int allword_frequency = 0;
-				int keyword_frequency = 0;
-				List<Keyword> kw = guessFromString(combine_st);
-				for (Keyword i : kw) {
-					int freq = i.getFrequency();
-					if (freq >= 10) {
-						System.out.println(i.getStem() + "\t" + freq);
-						keyword_frequency = keyword_frequency + freq;
-					}
-					allword_frequency = allword_frequency + freq;
+		// Apache Lucene: https://stackoverflow.com/questions/17447045/java-library-for-keywords-extraction-from-input-text
+		try {	// Identify Keywords and frequency
+			int allword_frequency = 0;
+			int keyword_frequency = 0;
+			List<Keyword> kw = guessFromString(combine_st);
+			for (Keyword i : kw) {
+				int freq = i.getFrequency();
+				if (freq >= 10) {
+					System.out.println(i.getStem() + "\t" + freq);
+					keyword_frequency = keyword_frequency + freq;
 				}
-				System.out.println(keyword_frequency);
-				System.out.println(allword_frequency);
-				double ratio = (double) keyword_frequency / (double) allword_frequency * 100;
-				System.out.println("selection vs all ratio = " + ratio + " %");
-
-				// Search using keyword
-				int records_hit_count = 0;
-//				String searh_word = "restrict*";
-//				String searh_word = "\"road* clos*\"~0";		// Lucene proximity search: https://lucene.apache.org/core/3_6_0/queryparsersyntax.html#Range%20Searches
-//				String searh_word = "\"no clos*\"~4 OR \"clos* none\"~4";
-//				String searh_word = "(area or road* OR highway* OR hwy OR motorway* OR route* OR trail*) AND clos* AND NOT(discontinu* OR lift* OR remove*)";		// discontinued, lifted, removed		except for, could be closed, no, none
-				String searh_word = "(area OR road* OR highway* OR hwy OR motorway* OR route* OR trail*) AND clos* AND NOT(discontinu* OR lift* OR remove*) AND NOT(\"no clos*\"~4 OR \"clos* none\"~4)";
-				int total_rows = row_data.size();
-				for (int row = 0; row < total_rows; row++) {
-					String st = row_data.get(row);
-					if (st != null) {
-						try {
-//							st = String.join(" ", analyze(st, new SimpleAnalyzer()));	// Test, No need it
-							// Reference:
-							// https://stackoverflow.com/questions/49066168/search-a-text-string-with-a-lucene-query-in-java
-							// https://www.baeldung.com/lucene-analyzers
-							// https://www.baeldung.com/lucene
-
-							// 0. Specify the analyzer for tokenizing text. The same analyzer should be used
-							// for indexing and searching
-							Analyzer analyzer = new SimpleAnalyzer();
-							TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(st));
-							tokenStream = new LowerCaseFilter(tokenStream); // to lower-case
-							tokenStream = new ASCIIFoldingFilter(tokenStream); // convert any char to ASCII
-//							tokenStream = new ClassicFilter(tokenStream); // remove dots from acronyms (and "'s" but already done manually above
-//							tokenStream = new StopFilter(tokenStream, EnglishAnalyzer.getDefaultStopSet());		// remove English stop words
-//							tokenStream = new PorterStemFilter(tokenStream);
-//							tokenStream = new SnowballFilter(tokenStream, "English");
-							
-//							List<String> filter_words = Arrays.asList("road", "close");
-//							CharArraySet chars = new CharArraySet(filter_words, false);
-//							tokenStream = new KeepWordFilter(tokenStream, chars);
-							
-//							ShingleFilter sf = new ShingleFilter(tokenStream, 2, 2);
-//							// sf.setOutputUnigrams(false);
-//							tokenStream = sf;
-							
-							
-							
-							
-
-							// 1. create the index
-							Directory index = new ByteBuffersDirectory();
-							IndexWriterConfig config = new IndexWriterConfig(analyzer);
-							IndexWriter w = new IndexWriter(index, config);
-							String[] line = st.split("\\.");
-							for (int l = 0; l < line.length; l++) {
-								Document doc = new Document();
-								String content = line[l];
-								String title = String.valueOf(row + 1) + "." + INC209R.get(row) + ".Line " + String.valueOf(l + 1);
-								doc.add(new StringField("title", title, Field.Store.YES)); // adding title field
-								doc.add(new TextField("content", content, Field.Store.YES)); // adding content field
-								w.addDocument(doc);
-							}
-							w.close();
-
-							// 2. query
-//							Query query = new QueryParser("content", analyzer).parse(searh_word);
-							Query query = new ComplexPhraseQueryParser("content", analyzer).parse(searh_word);
-
-							// 3. search
-							int hitsPerPage = 10;
-							IndexReader reader = DirectoryReader.open(index);
-							IndexSearcher searcher = new IndexSearcher(reader);
-							TopDocs docs = searcher.search(query, hitsPerPage);
-							ScoreDoc[] hits = docs.scoreDocs;
-
-							// 4. display results
-							int number_of_hits = hits.length;
-							if (number_of_hits > 0) {
-								System.out.println(number_of_hits + " hits.");
-								for (int i = 0; i < hits.length; ++i) {
-									int docId = hits[i].doc;
-									Document d = searcher.doc(docId);
-									System.out.println(d.get("title") + "\t" + d.get("content"));
-								}
-								records_hit_count++;
-							}
-
-							// 5. reader can only be closed when there is no need to access the documents
-							// any more.
-							reader.close();
-						} catch (ParseException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-				System.out.println("Number of records that contain the word '" + searh_word + "' = " + records_hit_count);
-			} catch (IOException e) {
-				e.printStackTrace();
+				allword_frequency = allword_frequency + freq;
 			}
+			System.out.println(keyword_frequency);
+			System.out.println(allword_frequency);
+			double ratio = (double) keyword_frequency / (double) allword_frequency * 100;
+			System.out.println("selection vs all ratio = " + ratio + " %");
+
+			// Search using keyword
+			int records_hit_count = 0;
+//			String searh_word = "restrict*";
+//			String searh_word = "\"road* clos*\"~0";		// Lucene proximity search: https://lucene.apache.org/core/3_6_0/queryparsersyntax.html#Range%20Searches
+//			String searh_word = "\"no clos*\"~4 OR \"clos* none\"~4";
+//			String searh_word = "(area or road* OR highway* OR hwy OR motorway* OR route* OR trail*) AND clos* AND NOT(discontinu* OR lift* OR remove*)";		// discontinued, lifted, removed		except for, could be closed, no, none
+			String searh_word = "(area OR road* OR highway* OR hwy OR motorway* OR route* OR trail*) AND clos* AND NOT(discontinu* OR lift* OR remove*) AND NOT(\"no clos*\"~4 OR \"clos* none\"~4)";
+			int total_rows = row_data.size();
+			for (int row = 0; row < total_rows; row++) {
+				String st = row_data.get(row);
+				if (st != null) {
+					try {
+//						st = String.join(" ", analyze(st, new SimpleAnalyzer()));	// Test, No need it
+						// Reference:
+						// https://stackoverflow.com/questions/49066168/search-a-text-string-with-a-lucene-query-in-java
+						// https://www.baeldung.com/lucene-analyzers
+						// https://www.baeldung.com/lucene
+
+						// 0. Specify the analyzer for tokenizing text. The same analyzer should be used
+						// for indexing and searching
+						Analyzer analyzer = new SimpleAnalyzer();
+						TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(st));
+						tokenStream = new LowerCaseFilter(tokenStream); // to lower-case
+						tokenStream = new ASCIIFoldingFilter(tokenStream); // convert any char to ASCII
+//						tokenStream = new ClassicFilter(tokenStream); // remove dots from acronyms (and "'s" but already done manually above
+//						tokenStream = new StopFilter(tokenStream, EnglishAnalyzer.getDefaultStopSet());		// remove English stop words
+//						tokenStream = new PorterStemFilter(tokenStream);
+//						tokenStream = new SnowballFilter(tokenStream, "English");
+						
+//						List<String> filter_words = Arrays.asList("road", "close");
+//						CharArraySet chars = new CharArraySet(filter_words, false);
+//						tokenStream = new KeepWordFilter(tokenStream, chars);
+						
+//						ShingleFilter sf = new ShingleFilter(tokenStream, 2, 2);
+//						// sf.setOutputUnigrams(false);
+//						tokenStream = sf;
+						
+						
+						
+						
+
+						// 1. create the index
+						Directory index = new ByteBuffersDirectory();
+						IndexWriterConfig config = new IndexWriterConfig(analyzer);
+						IndexWriter w = new IndexWriter(index, config);
+						String[] line = st.split("\\.");
+						for (int l = 0; l < line.length; l++) {
+							Document doc = new Document();
+							String content = line[l];
+							String title = String.valueOf(row + 1) + "." + INC209R.get(row) + ".Line " + String.valueOf(l + 1);
+							doc.add(new StringField("title", title, Field.Store.YES)); // adding title field
+							doc.add(new TextField("content", content, Field.Store.YES)); // adding content field
+							w.addDocument(doc);
+						}
+						w.close();
+
+						// 2. query
+//						Query query = new QueryParser("content", analyzer).parse(searh_word);
+						Query query = new ComplexPhraseQueryParser("content", analyzer).parse(searh_word);
+
+						// 3. search
+						int hitsPerPage = 10;
+						IndexReader reader = DirectoryReader.open(index);
+						IndexSearcher searcher = new IndexSearcher(reader);
+						TopDocs docs = searcher.search(query, hitsPerPage);
+						ScoreDoc[] hits = docs.scoreDocs;
+
+						// 4. display results
+						int number_of_hits = hits.length;
+						if (number_of_hits > 0) {
+							System.out.println(number_of_hits + " hits.");
+							for (int i = 0; i < hits.length; ++i) {
+								int docId = hits[i].doc;
+								Document d = searcher.doc(docId);
+								System.out.println(d.get("title") + "\t" + d.get("content"));
+							}
+							records_hit_count++;
+						}
+
+						// 5. reader can only be closed when there is no need to access the documents
+						// any more.
+						reader.close();
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			System.out.println("Number of records that contain the word '" + searh_word + "' = " + records_hit_count);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public List<String> analyze(String text, Analyzer analyzer) throws IOException{
