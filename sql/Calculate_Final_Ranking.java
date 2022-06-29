ddbc28c1ef4088dc99a4dc94a39f69b4bd012989
Tuan Dung Nguyen <dzung.csu@gmail.com>
2022-06-29 17:12:41 -0600

    show A2 ranking points in text area

--- sql/Calculate_Final_Ranking.java
+++ sql/Calculate_Final_Ranking.java
@@ -8,10 +8,68 @@
  */
 package sql;
 
+import java.awt.Dialog;
+import java.awt.Dimension;
+import java.awt.Window;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import convenience_classes.TextAreaReadMe;
+import convenience_classes.TitleScrollPane;
+import root.IMSRmain;
+
 public class Calculate_Final_Ranking {
+	int number_of_records;
+	Calculate_A2 a2;
 
 	public Calculate_Final_Ranking() {
-		Calculate_A2 a2 = new Calculate_A2();
+		a2 = new Calculate_A2();
 		
+		number_of_records = a2.year.size();
+		new Ranking_Points_Scroll();
+	}
+	
+	class Ranking_Points_Scroll extends JScrollPane {
+		public Ranking_Points_Scroll() {		
+			String[] header = new String[] { "RECORD", "YEAR", "INC", "INC209", "A2" };
+			TextAreaReadMe textarea = new TextAreaReadMe("icon_tree.png", 75, 75);	// Print to text area
+			textarea.append(String.join("\t", header)  + "\n");
+			
+			for (int i = 0; i < number_of_records; i++) {
+				textarea.append(String.valueOf(i+1) + "\t" + a2.year.get(i) + "\t" + a2.INC.get(i) + "\t" + a2.INC209R.get(i) + "\t" + a2.final_point.get(i)+ "\n");
+			}
+			
+			textarea.setSelectionStart(0);	// scroll to top
+			textarea.setSelectionEnd(0);
+			textarea.setEditable(false);
+			
+			TitleScrollPane explore_scrollpane = new TitleScrollPane("", "CENTER", textarea);
+			addHierarchyListener(new HierarchyListener() {	//	These codes make the license_scrollpane resizable --> the Big ScrollPane resizable --> JOptionPane resizable
+			    public void hierarchyChanged(HierarchyEvent e) {
+			        Window window = SwingUtilities.getWindowAncestor(explore_scrollpane);
+			        if (window instanceof Dialog) {
+			            Dialog dialog = (Dialog)window;
+			            if (!dialog.isResizable()) {
+			                dialog.setResizable(true);
+			                dialog.setPreferredSize(new Dimension((int) (IMSRmain.get_main().getWidth() / 1.1), (int) (IMSRmain.get_main().getHeight() / 1.21)));
+			            }
+			        }
+			    }
+			});
+			
+			// Add the Panel to this Big ScrollPane
+			setBorder(BorderFactory.createEmptyBorder());
+			setViewportView(explore_scrollpane);
+			
+			// Add everything to a popup panel
+			String ExitOption[] = {"EXIT" };
+			int response = JOptionPane.showOptionDialog(IMSRmain.get_DesktopPane(), this, "California Priority Points",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, ExitOption, ExitOption[0]);
+		}
 	}
 }
