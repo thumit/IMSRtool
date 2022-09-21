1ee5b8b909aff308c658d03a2e785061973bd4e1
Tuan Dung Nguyen <dzung.csu@gmail.com>
2022-09-21 07:16:35 -0600

    Fix failed adding fire name in 2012 data (checking the Hrs prefix)

--- root/ISMR_Process.java
+++ root/ISMR_Process.java
@@ -429,7 +429,7 @@ public class ISMR_Process {
 						fire_name = String.join(" ", fire_name, line_split.get(i)[id]);	// this is the incident name (or part of the name that is in the same line with other fire information), join by space
 					}
 					if (line_split.get(i - 1).length <= 2 || (line_split.get(i - 1).length <= 3 && line_split.get(i - 1)[0].contains("\\*"))) {
-						if (!lines[i - 1].startsWith("24 Hrs")) {	// this special check is needed for years < 2015
+						if (!lines[i - 1].startsWith("24 Hrs") && !lines[i - 1].startsWith("Hrs")) {	// this special check is needed for years < 2015. Note: (2013,2014: 24 Hrs) (2012: Hrs)
 							fire_name = String.join(" ", line_split.get(i - 1)) + " " + fire_name;	// join by space
 						}
 					}
